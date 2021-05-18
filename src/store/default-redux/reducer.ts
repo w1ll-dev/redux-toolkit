049828161c48f619b8ccc528e9b109c9673b512c
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-18 08:28:52 -0300

    create default redux increment reducer

---
+++ src/store/default-redux/reducer.ts
@@ -0,0 +1,12 @@
+import {Actions, Action} from './actions';
+
+export function counterReducer(state: number = 0, action: Action) {
+  switch (action.type) {
+    case Actions.INCREMENT:
+      return (state += 1);
+    case Actions.DECREMENT:
+      return (state -= 1);
+    default:
+      return state
+  }
+}
