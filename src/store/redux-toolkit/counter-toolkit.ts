75478fdd8c12817bd84c1d2b0fb7e2e459e4bf87
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-18 17:46:45 -0300

    create async reset action

--- src/store/redux-toolkit/counter-toolkit.ts
+++ src/store/redux-toolkit/counter-toolkit.ts
@@ -1,4 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
+import {AppDispatch, AppThunk} from '.';
 
 export const counterSlice = createSlice({
   initialState: {
@@ -12,8 +13,22 @@ export const counterSlice = createSlice({
     decrement(state) {
       state.counter -= 1;
     },
+    reset(state) {
+      state.counter = 0;
+    },
   },
 });
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export function asyncReset(): AppThunk {
+  return async function (dispatch: AppDispatch) {
+    await sleep(1500);
+    dispatch(counterSlice.actions.reset());
+  };
+}
 
 export const {increment, decrement} = counterSlice.actions;
 export const counterReducer = counterSlice.reducer;
