be6a1062110d631e7c1eaaae33b4ec662dc81050
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-18 17:46:21 -0300

    add AppDispatch, AppThunk and useAppDispatch to redux toolkit

--- src/store/redux-toolkit/index.ts
+++ src/store/redux-toolkit/index.ts
@@ -1,4 +1,5 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit';
+import {useDispatch} from 'react-redux';
 import {counterReducer} from './counter-toolkit';
 
 export const toolkitStore = configureStore({
@@ -6,3 +7,7 @@ export const toolkitStore = configureStore({
 });
 
 export type ToolkitType = ReturnType<typeof toolkitStore.getState>;
+
+export type AppDispatch = typeof toolkitStore.dispatch;
+export type AppThunk = ThunkAction<void, ToolkitType, null, Action<string>>;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
